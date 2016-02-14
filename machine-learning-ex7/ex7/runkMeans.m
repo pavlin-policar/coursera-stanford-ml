5737e183778f859c0f57e6b6d09218c6b38a8721
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-02-14 16:01:15 +0000

    Completed ex7 - kmeans and PCA

--- machine-learning-ex7/ex7/runkMeans.m
+++ machine-learning-ex7/ex7/runkMeans.m
@@ -1,43 +1,43 @@
 function [centroids, idx] = runkMeans(X, initial_centroids, ...
                                       max_iters, plot_progress)
-%RUNKMEANS runs the K-Means algorithm on data matrix X, where each row of X
-%is a single example
-%   [centroids, idx] = RUNKMEANS(X, initial_centroids, max_iters, ...
-%   plot_progress) runs the K-Means algorithm on data matrix X, where each 
-%   row of X is a single example. It uses initial_centroids used as the
-%   initial centroids. max_iters specifies the total number of interactions 
-%   of K-Means to execute. plot_progress is a true/false flag that 
-%   indicates if the function should also plot its progress as the 
-%   learning happens. This is set to false by default. runkMeans returns 
-%   centroids, a Kxn matrix of the computed centroids and idx, a m x 1 
-%   vector of centroid assignments (i.e. each entry in range [1..K])
-%
+  % RUNKMEANS runs the K-Means algorithm on data matrix X, where each row of X
+  % is a single example
+  %   [centroids, idx] = RUNKMEANS(X, initial_centroids, max_iters, ...
+  %   plot_progress) runs the K-Means algorithm on data matrix X, where each 
+  %   row of X is a single example. It uses initial_centroids used as the
+  %   initial centroids. max_iters specifies the total number of interactions 
+  %   of K-Means to execute. plot_progress is a true/false flag that 
+  %   indicates if the function should also plot its progress as the 
+  %   learning happens. This is set to false by default. runkMeans returns 
+  %   centroids, a Kxn matrix of the computed centroids and idx, a m x 1 
+  %   vector of centroid assignments (i.e. each entry in range [1..K])
+  %
 
-% Set default value for plot progress
-if ~exist('plot_progress', 'var') || isempty(plot_progress)
+  % Set default value for plot progress
+  if ~exist('plot_progress', 'var') || isempty(plot_progress)
     plot_progress = false;
-end
+  end
 
-% Plot the data if we are plotting progress
-if plot_progress
+  % Plot the data if we are plotting progress
+  if plot_progress
     figure;
     hold on;
-end
+  end
 
-% Initialize values
-[m n] = size(X);
-K = size(initial_centroids, 1);
-centroids = initial_centroids;
-previous_centroids = centroids;
-idx = zeros(m, 1);
+  % Initialize values
+  [m n] = size(X);
+  K = size(initial_centroids, 1);
+  centroids = initial_centroids;
+  previous_centroids = centroids;
+  idx = zeros(m, 1);
 
-% Run K-Means
-for i=1:max_iters
+  % Run K-Means
+  for i=1:max_iters
     
     % Output progress
     fprintf('K-Means iteration %d/%d...\n', i, max_iters);
     if exist('OCTAVE_VERSION')
-        fflush(stdout);
+      fflush(stdout);
     end
     
     % For each example in X, assign it to the closest centroid
@@ -45,20 +45,20 @@ for i=1:max_iters
     
     % Optionally, plot progress here
     if plot_progress
-        plotProgresskMeans(X, centroids, previous_centroids, idx, K, i);
-        previous_centroids = centroids;
-        fprintf('Press enter to continue.\n');
-        pause;
+      plotProgresskMeans(X, centroids, previous_centroids, idx, K, i);
+      previous_centroids = centroids;
+      fprintf('Press enter to continue.\n');
+      pause;
     end
     
     % Given the memberships, compute new centroids
     centroids = computeCentroids(X, idx, K);
-end
+  end
 
-% Hold off if we are plotting progress
-if plot_progress
+  % Hold off if we are plotting progress
+  if plot_progress
     hold off;
-end
+  end
 
 end
 
